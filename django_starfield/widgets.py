02e589de66b387ab23f90067958f7f8d8ec7d57d
Dominik George <nik@naturalnet.de>
2018-08-02 12:29:23 +0200

    Add codepoint attribut to widget.

--- django_starfield/widgets.py
+++ django_starfield/widgets.py
@@ -3,9 +3,10 @@ from django.forms.widget import Widget
 class Stars(Widget):
     template_name = 'django_starfield/stars.html'
 
-    def __init__(self, attrs=None, stars=5):
+    def __init__(self, attrs=None, stars=5, codepoint='2605'):
         super(Stars, self).__init__(attrs)
         self.stars = 5
+        self.codepoint = '2605'
 
     def get_context(self, name, value, attrs):
         context = super(Stars, self).get_context(name, value, attrs)
