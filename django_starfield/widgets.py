45a6509f5c0291e10cb52b16eef98fd5103718e8
Dominik George <nik@naturalnet.de>
2018-08-02 14:20:18 +0200

    Make codepoint and colour properly configurable.

--- django_starfield/widgets.py
+++ django_starfield/widgets.py
@@ -1,15 +1,28 @@
+from django.config import settings
 from django.forms.widgets import Widget
+
+from . import defaults
+
+settings.configure(default_settings=defaults)
 
 class Stars(Widget):
     template_name = 'django_starfield/stars.html'
 
-    def __init__(self, attrs=None, stars=5, codepoint='2605'):
+    def __init__(self, attrs=None, stars=None, codepoint=None, colour=None):
         super(Stars, self).__init__(attrs)
-        self.stars = 5
-        self.codepoint = codepoint
+
+        if stars is None:
+            self.stars = settings.STARFIELD_STARS
+        if codepoint is None:
+            self.codepoint = settings.STARFIELD_CODEPOINT
+        if colour is None:
+            self.colour = settings.STARFIELD_COLOUR
 
     def get_context(self, name, value, attrs):
         context = super(Stars, self).get_context(name, value, attrs)
         context['stars'] = range(self.stars, 0, -1)
-        context['codepoint'] = self.codepoint
+        if not self.codepoint == defaults.STARFIELD_CODEPOINT:
+            context['codepoint'] = self.codepoint
+        if not self.colour == defaults.STARFIELD_COLOUR:
+            contect['colour'] = self.colour
         return context
