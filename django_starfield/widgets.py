efa470820654c8e6e7dbb32a04ddad80c5401ee2
Dominik George <nik@naturalnet.de>
2018-08-02 14:30:11 +0200

    Correctly set default settings.

--- django_starfield/widgets.py
+++ django_starfield/widgets.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.forms.widgets import Widget
 
-from . import defaults
-
-settings.configure(default_settings=defaults)
+STARFIELD_COLOUR = '#f5b301'
+STARFIELD_CODEPOINT = '2605'
+STARFIELD_STARS = 5
 
 class Stars(Widget):
     template_name = 'django_starfield/stars.html'
@@ -12,17 +12,17 @@ class Stars(Widget):
         super(Stars, self).__init__(attrs)
 
         if stars is None:
-            self.stars = settings.STARFIELD_STARS
+            self.stars = getattr(settings, 'STARFIELD_STARS', STARFIELD_STARS)
         if codepoint is None:
-            self.codepoint = settings.STARFIELD_CODEPOINT
+            self.codepoint = getattr(settings, 'STARFIELD_CODEPOINT', STARFIELD_CODEPOINT)
         if colour is None:
-            self.colour = settings.STARFIELD_COLOUR
+            self.colour = getattr(settings, 'STARFIELD_COLOUR', STARFIELD_COLOUR)
 
     def get_context(self, name, value, attrs):
         context = super(Stars, self).get_context(name, value, attrs)
         context['stars'] = range(self.stars, 0, -1)
-        if not self.codepoint == defaults.STARFIELD_CODEPOINT:
+        if not self.codepoint == STARFIELD_CODEPOINT:
             context['codepoint'] = self.codepoint
-        if not self.colour == defaults.STARFIELD_COLOUR:
+        if not self.colour == STARFIELD_COLOUR:
             contect['colour'] = self.colour
         return context
