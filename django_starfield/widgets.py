19888a8575401341a0fea2a04beb810bdf5f7dc3
andargunov <tolik_azo@mail.ru>
2019-09-26 19:07:05 +0300

    кастомные стили

--- django_starfield/widgets.py
+++ django_starfield/widgets.py
@@ -4,27 +4,30 @@ import random
 import string
 
 STARFIELD_COLOUR = '#f5b301'
-STARFIELD_CODEPOINT = '2605'
+STARFIELD_CONTENT = "'\\2605'"
 STARFIELD_STARS = 5
 STARFIELD_TEMPLATE = 'django_starfield/stars.html'
+STARFIELD_CSS = "django_starfield.css"
 
 class Stars(Widget):
     template_name = getattr(settings, 'STARFIELD_TEMPLATE', STARFIELD_TEMPLATE)
 
-    def __init__(self, attrs=None, stars=None, codepoint=None, colour=None, unique_id=None):
+    def __init__(self, attrs=None, stars=None, content=None, colour=None, unique_id=None, css_file=None):
         super(Stars, self).__init__(attrs)
 
         self.stars = stars if stars else getattr(settings, 'STARFIELD_STARS', STARFIELD_STARS)
-        self.codepoint = codepoint if codepoint else getattr(settings, 'STARFIELD_CODEPOINT', STARFIELD_CODEPOINT)
+        self.content = content if content else getattr(settings, 'STARFIELD_CONTENT', STARFIELD_CONTENT)
         self.colour = colour if colour else getattr(settings, 'STARFIELD_COLOUR', STARFIELD_COLOUR)
+        self.css_file = css_file if css_file else getattr(settings, 'STARFIELD_CSS', STARFIELD_CSS)
         self.unique_id = unique_id if unique_id else ''.join([random.choice(string.ascii_lowercase) for i in range(8)])
 
     def get_context(self, name, value, attrs):
         context = super(Stars, self).get_context(name, value, attrs)
         context['stars'] = range(self.stars, 0, -1)
         context['unique_id'] = self.unique_id
-        if not self.codepoint == STARFIELD_CODEPOINT:
-            context['codepoint'] = self.codepoint
+        context['css_file'] = self.css_file
+        if not self.content == STARFIELD_CONTENT:
+            context['content'] = self.content
         if not self.colour == STARFIELD_COLOUR:
             context['colour'] = self.colour
         return context
